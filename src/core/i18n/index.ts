2515536299dccb399543c5b2b2b6d9a9ce0ed168
dmitryprogrammer <dpischalka@gmail.com>
2023-03-29 20:17:05 +0400

    [multilanguage] init i18n.

---
+++ src/core/i18n/index.ts
@@ -0,0 +1,11 @@
+import i18next from "i18next";
+import {initReactI18next} from "react-i18next";
+import I18NextHttpBackend from "i18next-http-backend";
+
+i18next
+  .use(I18NextHttpBackend)
+  .use(initReactI18next)
+  .init({
+    lng: "ru",
+    interpolation: {escapeValue: false}
+  });
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {App} from "./app/App";
 import "./assets/styles/index.scss";
+import "./core/i18n";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
