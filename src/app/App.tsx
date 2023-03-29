c577f4d70719925805b8d807784de57d9068628a
dmitryprogrammer <dpischalka@gmail.com>
2023-03-29 20:17:45 +0400

    [multilanguage] added first translations for title and put it in App component.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,26 @@
+import {useTranslation} from "react-i18next";
+
+enum LANGUAGES {
+  ru = "ru",
+  en = "en"
+}
+
 export function App() {
+  const {t, i18n} = useTranslation();
+
+  const switchLanguage = (language: LANGUAGES): void => {
+    i18n.changeLanguage(language);
+  };
+
   return (
-    <>
-      <h1>App</h1>
-      <main>Content</main>
-    </>
-  )
+    <main className="container">
+      <h1 className="title">{t("title")}</h1>
+
+      Content
+      <div>
+        <button onClick={() => switchLanguage(LANGUAGES.en)}>{LANGUAGES.en}</button>
+        <button onClick={() => switchLanguage(LANGUAGES.ru)}>{LANGUAGES.ru}</button>
+      </div>
+    </main>
+  );
 }
