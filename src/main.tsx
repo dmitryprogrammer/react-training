83af5aac39126950298ec1f07b8243a8d2161b54
dmitryprogrammer <dpischalka@gmail.com>
2023-04-05 10:33:37 +0400

    [navigation-building] implemented routing to "main.tsx".

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {RouterProvider} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 import "./assets/styles/index.scss";
 import "./core/i18n";
-import {ROUTER_CONFIG} from "./navigation/router-config";
+import {RouterConfig} from "./navigation";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={ROUTER_CONFIG} />
+    <BrowserRouter>
+      <RouterConfig />
+    </BrowserRouter>
   </React.StrictMode>
 );
