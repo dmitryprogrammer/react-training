e392d763548259d229726acbe4c317ba7d6503ef
dmitryprogrammer <dpischalka@gmail.com>
2023-04-12 01:21:42 +0400

    [navigation-building] created State and Effect routes components.

---
+++ src/pages/effect/effect.tsx
@@ -0,0 +1,8 @@
+export function Effect() {
+  return (
+    <>
+      <h2 className="title is-4">Effect</h2>
+      <p>This is the effect hook page</p>
+    </>
+  );
+}
---
+++ src/pages/state/state.tsx
@@ -0,0 +1,8 @@
+export function State() {
+  return (
+    <>
+      <h2 className="title is-4">State</h2>
+      <p>This is the state hook page</p>
+    </>
+  );
+}
