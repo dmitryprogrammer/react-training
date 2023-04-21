323aa9bf24ef17be8a6e23c8efbdfa450df4a971
dmitryprogrammer <dpischalka@gmail.com>
2023-04-22 01:15:28 +0400

    [effect-hook-practice] started effect-hook-practice.

--- src/pages/effect/effect.tsx
+++ src/pages/effect/effect.tsx
@@ -7,6 +7,9 @@ export function Effect() {
     <>
       <h2 className="title is-4">{t("effectHook.title")}</h2>
       <p>{t("effectHook.description")}</p>
+      <div className="buttons mt-4">
+        <button className="button is-info">Click for increase</button>
+      </div>
     </>
   );
 }
