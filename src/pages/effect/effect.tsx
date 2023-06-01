84e1e35b28322d6f7dee040797ebde5da289141f
dmitryprogrammer <dpischalka@gmail.com>
2023-06-01 19:00:02 +0400

    [effect-hook-practice] first effect.

--- src/pages/effect/effect.tsx
+++ src/pages/effect/effect.tsx
@@ -1,15 +1,36 @@
 import {useTranslation} from "react-i18next";
+import {useEffect, useState} from "react";
 
 export function Effect() {
+  const buttonClasses = {
+    1: "is-info",
+    4: "is-success",
+    6: "is-warning",
+    7: "is-danger"
+  };
   const {t} = useTranslation();
+  const [clickCount, setClickCount] = useState(0);
+  const [increaseButtonClass, setIncreaseButtonClass] = useState("is-info");
+
+  useEffect(() => {
+    for (const buttonClass in buttonClasses) {
+      if (clickCount === Number(buttonClass)) {
+        setIncreaseButtonClass(buttonClasses[buttonClass]);
+      }
+    }
+  });
 
   return (
     <>
       <h2 className="title is-4">{t("effectHook.title")}</h2>
       <p>{t("effectHook.description")}</p>
       <div className="buttons mt-4">
-        <button className="button is-info">Click for increase</button>
+        <button className={"button " + increaseButtonClass} onClick={() => setClickCount(clickCount + 1)}>Click for
+          increase
+        </button>
       </div>
+
+      <span>You clicked increase {clickCount} times</span>
     </>
   );
 }
