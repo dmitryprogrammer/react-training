504544ad86c5bb0fe1133891d3381e162597b242
dmitryprogrammer <dpischalka@gmail.com>
2024-07-03 00:39:36 +0400

    [calc] created ratio component

--- src/pages/calc/calc.tsx
+++ src/pages/calc/calc.tsx
@@ -1,5 +1,10 @@
+import {Ration} from "./ratio";
+
 export const Calc = () => {
   return <>
     <h2>Calc Component</h2>
+    <form className="block mt-4">
+        <Ration />
+    </form>
   </>
 }
---
+++ src/pages/calc/ratio.tsx
@@ -0,0 +1,7 @@
+export const Ration = () => {
+  return <div className="field">
+    <label htmlFor="name_control" className="label">ratio</label>
+
+    <input type="text" className="input control"/>
+  </div>
+}
