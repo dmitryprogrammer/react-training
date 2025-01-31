e6fcc4677de03ccf45825dc22cc2e57803762d66
dmitryprogrammer <dpischalka@gmail.com>
2025-01-31 14:31:55 +0300

    feat(effect): added decrease button, added disable flags for buttons.

--- src/pages/effect/effect.tsx
+++ src/pages/effect/effect.tsx
@@ -8,9 +8,13 @@ export function Effect() {
     8: "is-warning",
     15: "is-danger",
   };
+  const MAX_COUNT = 20;
+  const MIN_COUNT = 0;
   const {t} = useTranslation(null, {keyPrefix: "effectHook"});
   const [clickCount, setClickCount] = useState(0);
   const [increaseButtonClass, setIncreaseButtonClass] = useState("is-info");
+  const [disableIncBtn, setDisableIncBtn] = useState(false);
+  const [disableDeincBtn, setDisabledeincBtn] = useState(false);
 
   useEffect(() => {
     for (const buttonClass in buttonClasses) {
@@ -18,15 +22,22 @@ export function Effect() {
         setIncreaseButtonClass(buttonClasses[buttonClass]);
       }
     }
-  });
+    clickCount >= MAX_COUNT ? setDisableIncBtn(true) : setDisableIncBtn(false);
+    clickCount <= MIN_COUNT ? setDisabledeincBtn(true) : setDisabledeincBtn(false);
+  }, [clickCount]);
 
   return (
     <>
       <h2 className="title is-4">{t("title")}</h2>
       <p>{t("description")}</p>
       <div className="buttons mt-4">
-        <button className={"button " + increaseButtonClass}
-                onClick={() => setClickCount(clickCount + 1)}>{t("increaseButton")}
+        <button disabled={disableIncBtn} className={"button " + increaseButtonClass} onClick={() => setClickCount(clickCount + 1)}>
+          {t("increaseButton")}
+        </button>
+      </div>
+      <div className="buttons mt-4">
+        <button disabled={disableDeincBtn} className={"button " + increaseButtonClass} onClick={() => setClickCount(clickCount - 1)}>
+          {t("decreaseButton")}
         </button>
       </div>
 
